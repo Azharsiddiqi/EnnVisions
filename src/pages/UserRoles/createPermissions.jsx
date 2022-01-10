258d67951284b2e5e138f10fa25c368ff1147860
Arslan Majeed <arslanmajeed270@gmail.com>
2022-01-10 14:05:08 +0500

    removed typescript plugin from eslint

--- src/pages/UserRoles/createPermissions.jsx
+++ src/pages/UserRoles/createPermissions.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { PRIVATE_ROUTES } from "../../config";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { PRIVATE_ROUTES } from '../../config';
+import { useNavigate } from 'react-router-dom';
 
 const createPermissions = () => {
   const navigate = useNavigate();
